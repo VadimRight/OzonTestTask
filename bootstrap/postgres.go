ed8ce686a893abe5bb62da76882bb927db6edd4e
VadimRight <vadimright@mail.ru>
2024-05-22 12:22:03 +0300

    Fit create query for migration is ready

--- bootstrap/postgres.go
+++ bootstrap/postgres.go
@@ -7,10 +7,12 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// Тип базы данных
 type Storage struct {
 	db *sql.DB
 }
 
+// Функция инициализации базы данных и подключение к базе данных
 func InitPostgresDatabase(cfg *Config) *Storage  {
 	const op = "postgres.InitPostgresDatabase"
 
@@ -30,6 +32,21 @@ func InitPostgresDatabase(cfg *Config) *Storage  {
 		id UUID PRIMARY KEY,
 		username VARCHAR(20) NOT NULL UNIQUE,
 		password CHAR(60) NOT NULL UNIQUE
+	)
+	/
+	CREATE TABLE IF NOT EXISTS post (
+		id UUID PRIMARY KEY,
+		text TEXT NOT NULL,
+		author_id UUID NOT NULL,
+		FOREIGN KEY (author_id) REFERENCES "user"(id)
+
+	)
+	/
+	CREATE TABLE IF NOT EXISTS comment (
+		id UUID PRIMARY KEY,
+		comment VARCHAR(2000),
+		author_id UUID NOT NULL,
+		FOREIGN KEY (author_id) REFERENCES "user"(id),
 	);`)
 	if err != nil {
 		log.Fatalf("%s: %v", op, err)
@@ -41,6 +58,7 @@ func InitPostgresDatabase(cfg *Config) *Storage  {
 	return &Storage{db: db}
 }
 
+// Функция закрытия соединения с базой данных
 func CloseDB(db *Storage) error {
 	return db.db.Close()
 }
